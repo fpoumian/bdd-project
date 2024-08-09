4067fb296d5b688e76d1e2bd2d325196487afd6a
Fernando Poumian <ksquare@Ksquares-MacBook-Pro.local>
2024-08-09 15:41:11 -0600

    add test for image search functionality

--- cypress/e2e/features/app.feature
+++ cypress/e2e/features/app.feature
@@ -3,3 +3,6 @@ Feature: Snapshot website test scenarios
     Given I visit the home page
     Then the header should be visible
     Then I should be redirected to the Mountain Pictures search
+    Then I click in the text input
+    Then I type birds into the search input and press search button
+    Then I should see Birds Images in header
--- cypress/e2e/step_definitions/app.js
+++ cypress/e2e/step_definitions/app.js
@@ -10,4 +10,17 @@ Then("the header should be visible", () => {
 
 Then("I should be redirected to the Mountain Pictures search", () => {
     cy.get("h2").should("have.text", "mountain Pictures")
-})
+});
+
+Then("I click in the text input", () => {
+    cy.get("input").click();
+});
+
+Then("I type birds into the search input and press search button", () => {
+    cy.get("input").type("birds");
+    cy.get(".search-button").click();
+});
+
+Then("I should see Birds Images in header", () => {
+    cy.get(":nth-child(24) > img").should("be.visible");
+});
