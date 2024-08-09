4d4c9530eefa29afef63021037e9ea852bcb6f97
Fernando Poumian <ksquare@Ksquares-MacBook-Pro.local>
2024-08-09 13:28:11 -0600

    add test to make sure user is redirected to Mountains page from Home page

--- cypress/e2e/features/app.feature
+++ cypress/e2e/features/app.feature
@@ -2,3 +2,4 @@ Feature: Snapshot website test scenarios
   Scenario: visiting the home page - successful search
     Given I visit the home page
     Then the header should be visible
+    Then I should be redirected to the Mountain Pictures search
--- cypress/e2e/step_definitions/app.js
+++ cypress/e2e/step_definitions/app.js
@@ -7,3 +7,7 @@ Given("I visit the home page", () => {
 Then("the header should be visible", () => {
     cy.get("h1").should("be.visible");
 });
+
+Then("I should be redirected to the Mountain Pictures search", () => {
+    cy.get("h2").should("have.text", "mountain Pictures")
+})
